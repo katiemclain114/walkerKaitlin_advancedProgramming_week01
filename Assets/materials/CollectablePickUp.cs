9443ad853b251b2adbb3d372212d21dc186a4139
Kaitlin Walker <katiemclain114@gmail.com>
2022-01-29 14:53:56 -0500

    hour 1
    added player with movement and started collectable

---
+++ Assets/materials/CollectablePickUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablePickUp : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("pick up cube");
+        }
+    }
+}
---
+++ Assets/materials/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private float speedPlayer = .1f;
+    private Rigidbody rbPlayer;
+
+    private void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float xDirection = Input.GetAxisRaw("Horizontal");
+        float zDirection = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0, zDirection);
+
+        rbPlayer.AddRelativeForce(moveDirection * speedPlayer);
+
+        
+    }
+}
